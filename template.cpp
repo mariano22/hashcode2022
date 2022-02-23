f1b77eb14b6e5dd8fa46f73197f64ec3195255db
Mariano Crosetti <marianocrosetti1993@gmail.com>
2022-02-23 20:09:10 -0300

    Initial setup

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int,int> ii;
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define DEBUG
+#ifdef DEBUG
+    #define dprint(v) cout << #v"=" << v << endl //;)
+    #define dpr(v) cout << #v"=" << v << endl //;)
+    #define dpra(a,n) { forn(i,(n)) cout << (a)[i] << (i==(n)-1?'\n':' '); }
+    #define dprv(vec) dpra(vec,sz(vec))
+
+#else
+    #define dprint(v) (void)0
+    #define dpr(v) (void)0
+    #define dpra(a,n) (void)0
+    #define dprv(vec) (void)0
+#endif
+
+const int MAXN=100100;
+int n;
+
+int main() {
+    #ifdef DEBUG
+        freopen("in0", "r", stdin);
+    #endif
+    ios::sync_with_stdio(0);
+    while(cin >> n){
+    
+    }
+    return 0;
+}
