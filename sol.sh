8d478eddd8991d1b88dd6510e5b25b703b4d9260
Mariano Crosetti <marianocrosetti1993@gmail.com>
2022-02-23 20:51:01 -0300

    update

--- sol.sh
+++ sol.sh
@@ -7,6 +7,8 @@ g++ -O3 -w -o $SOL.out -DDEBUG -std=c++17 $SOL.cpp
 chmod +x $SOL.out
 
 for filepath in ./in/*.in; do
+    echo ""
+    echo ""
     filename="${filepath##*/}"
     echo "CASE $filename"
     time ./$SOL.out < $filepath > ./out/${filename%.*}.out
