97c7069a6f91db238d74de398415f562e6894041
Mariano Crosetti <marianocrosetti1993@gmail.com>
2022-02-27 13:02:09 -0300

    Update script

--- sol.sh
+++ sol.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 SOL=$1
+OUT=$2
 
 echo "Using solution ${SOL}.cpp"
 g++ -O3 -w -o $SOL.out -std=c++17 $SOL.cpp
 chmod +x $SOL.out
 
-for filepath in ./in/*.in.txt; do
+rm -rf ./$OUT/
+mkdir ./$OUT
+cp ./$SOL.cpp ./$OUT
+
+for filepath in ./in/*.in; do
     echo ""
     echo ""
     filename="${filepath##*/}"
     echo "CASE $filename"
-    time ./$SOL.out < $filepath > ./out/${filename%.*}.out
+    time ./$SOL.out < $filepath > ./$OUT/${filename%.*}.out
 done
 
